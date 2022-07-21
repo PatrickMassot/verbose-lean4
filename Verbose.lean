195b9798d05a45e8590aaf9cf6eb393be7665caa
Patrick Massot <patrickmassot@free.fr>
2022-07-21 14:41:37 -0400

    Fix lakefile

---
+++ Verbose.lean
@@ -0,0 +1,3 @@
+import Verbose.Common
+import Verbose.Fix
+import Verbose.By
--- lakefile.lean
+++ lakefile.lean
@@ -8,11 +8,6 @@ package verbose {
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"@"a7a8e463eb56c18d55ddaac7e75d9cad6bee99b3"
 
-lean_lib Verbose {
-  -- add library configuration options here
-}
-
 @[defaultTarget]
-lean_exe verbose {
-  root := `Main
+lean_lib Verbose {
 }
