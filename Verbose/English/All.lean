49b7ef6af242c08cf08bb953f5d4570ff6d11e75
Patrick Massot <patrickmassot@free.fr>
2023-11-16 23:07:52 -0500

    Add file importing all tactics

---
+++ Verbose/English/All.lean
@@ -0,0 +1,7 @@
+import Verbose.English.Assume
+import Verbose.English.By
+import Verbose.English.Claim
+import Verbose.English.Fix
+import Verbose.English.Lets
+import Verbose.English.Set
+import Verbose.English.We
