39f9a0911cc145d6fdf0ff60594607bdb810e3b0
Patrick Massot <patrickmassot@free.fr>
2023-11-17 17:13:04 -0500

    Unlock more examples

--- Verbose/English/Examples.lean
+++ Verbose/English/Examples.lean
@@ -45,9 +45,8 @@ example (hl : l > 0) : sequence_tendsto u l → ∃ N, ∀ n ≥ N, u n ≥ l/2 
   By h applied to [l/2, half_pos hl] we get N (hN : ∀ n ≥ N, |u n - l| ≤ l / 2)
   Let's prove that N works
   Fix n ≥ N
-  By hN applied to [n, (n_ge : n ≥ N)]
-     we get (hN' : |u n - l| ≤ l / 2)
-  We rewrite using abs_le at hN'
+  By hN applied to n using (n_ge : n ≥ N) we get hN' : |u n - l| ≤ l / 2
+  We rewrite using abs_le at hN' which becomes -(l / 2) ≤ u n - l ∧ u n - l ≤ l / 2
   We conclude by hN'
 
 /-
@@ -67,10 +66,11 @@ sequence_tendsto (u + v) (l + l') := by
   Fact fait₂ : |v n - l'| ≤ ε/2
     We conclude by hN₂ applied to [n, hn₂]
   calc
-  |(u + v) n - (l + l')| = |(u n - l) + (v n - l')| : by We compute
-                     ... ≤ |u n - l| + |v n - l'| : by We apply abs_add
-                     ... ≤  ε/2 + ε/2             : by We combine [fait₁, fait₂]
-                     ... =  ε                     : by We compute
+  |(u + v) n - (l + l')| = |(u n - l) + (v n - l')| := by We compute
+                     _ ≤ |u n - l| + |v n - l'| := by We apply abs_add
+                     _ ≤  ε/2 + ε/2             := by We combine [fait₁, fait₂]
+                     _ =  ε                     := by We compute
+ -/
 
 example (hu : sequence_tendsto u l) (hw : sequence_tendsto w l)
 (h : ∀ n, u n ≤ v n)
@@ -86,35 +86,35 @@ example (hu : sequence_tendsto u l) (hw : sequence_tendsto w l)
   By hN' applied to [n, hn'] we get (hN'₁ : |w n - l| ≤ ε)
   By h applied to n we get (h₁ : u n ≤ v n)
   By h' applied to n we get (h'₁ : v n ≤ w n)
-  We rewrite using [abs_le] everywhere
+  We rewrite using abs_le everywhere
   By hN₁ we get (hNl : -ε ≤ u n - l) hNd
   By hN'₁ we get hN'l (hN'd : w n - l ≤ ε)
-  Let's prove that -ε ≤ v n - l
-  calc -ε ≤ u n - l : by We conclude by hNl
-      ... ≤ v n - l : by We conclude by h₁
+  Let's first prove that -ε ≤ v n - l
+  calc -ε ≤ u n - l := by We conclude by hNl
+      _ ≤ v n - l := by We conclude by h₁
   Let's prove that v n - l ≤ ε
-  calc v n - l ≤ w n - l : by We conclude by h'₁
-      ... ≤ ε : by We conclude by hN'd
+  calc v n - l ≤ w n - l := by We conclude by h'₁
+      _ ≤ ε := by We conclude by hN'd
 
 example (u l) : sequence_tendsto u l ↔
  ∀ ε > 0, ∃ N, ∀ n ≥ N, |u n - l| < ε := by
   Let's prove that sequence_tendsto u l → ∀ (ε : ℝ), ε > 0 → (∃ (N : ℕ), ∀ (n : ℕ), n ≥ N → |u n - l| < ε)
   Assume hyp : sequence_tendsto u l
-  Fix ε > 0,
-  By hyp applied to [ε/2, half_pos ε_pos] we get N
+  Fix ε > 0
+  By hyp applied to ε/2 using half_pos ε_pos we get N
       such that hN : ∀ (n : ℕ), n ≥ N → |u n - l| ≤ ε / 2
   Let's prove that N works
   Fix n ≥ N
-  calc |u n - l| ≤ ε/2 : by We conclude by hN applied to [n, n_ge]
-       ...       < ε : by We conclude by ε_pos
+  calc |u n - l| ≤ ε/2 := by We conclude by hN applied to [n, n_ge]
+       _       < ε := by We conclude by ε_pos
 
   Assume hyp : ∀ (ε : ℝ), ε > 0 → (∃ N, ∀ n ≥ N, |u n - l| < ε)
   Fix ε > 0
   By hyp applied to [ε, ε_pos] we get N such that hN : ∀ n ≥ N, |u n - l| < ε
   Let's prove that N works
   Fix n ≥ N
-  We conclude by hN applied to [n, n_ge],
-
+  We conclude by hN applied to n using n_ge
+/-
 example : sequence_tendsto u l → sequence_tendsto u l' → l = l' := by
   Assume (hl : sequence_tendsto u l) (hl' : sequence_tendsto u l')
   By eq_of_forall_dist_le it suffices to prove that ∀ (ε : ℝ), ε > 0 → |l - l'| ≤ ε
@@ -127,12 +127,12 @@ example : sequence_tendsto u l → sequence_tendsto u l' → l = l' := by
      we get hN₁ : |u (max N N') - l| ≤ ε / 2
   By hN' applied to [max N N', le_max_right _ _]
     we get hN'₁ : |u (max N N') - l'| ≤ ε / 2
-  calc |l - l'| = |(l-u (max N N')) + (u (max N N') -l')| : by We compute
-  ... ≤ |l - u (max N N')| + |u (max N N') - l'| : by We apply abs_add
-  ... =  |u (max N N') - l| + |u (max N N') - l'| : by We rewrite using abs_sub_comm
-  ... ≤ ε/2 + ε/2 : by We combine [hN₁, hN'₁]
-  ... = ε : by We compute
-
+  calc |l - l'| = |(l-u (max N N')) + (u (max N N') -l')| := by We compute
+  _ ≤ |l - u (max N N')| + |u (max N N') - l'| := by We apply abs_add
+  _ =  |u (max N N') - l| + |u (max N N') - l'| := by We rewrite using abs_sub_comm
+  _ ≤ ε/2 + ε/2 := by We combine [hN₁, hN'₁]
+  _ = ε := by We compute
+ -/
 def increasing (u : ℕ → ℝ) := ∀ n m, n ≤ m → u n ≤ u m
 
 def is_supremum (M : ℝ) (u : ℕ → ℝ) :=
@@ -149,9 +149,8 @@ sequence_tendsto u M := by
   By inf_M applied to n we get (inf_M' : u n ≤ M)
 
   We rewrite using [abs_le]
-  Let's prove that -ε ≤ u n - M
-    By h' applied to [n₀, n, n_ge] we get h'' : u n₀ ≤ u n
+  Let's first prove that -ε ≤ u n - M
+  · By h' applied to [n₀, n, n_ge] we get h'' : u n₀ ≤ u n
     We combine [h'', hn₀]
   Let's prove that u n - M ≤ ε
-  We combine [inf_M', ε_pos]
- -/
+  ·  We combine [inf_M', ε_pos]
