41ef61bbee83407943957d4316ed918c618c40d7
Patrick Massot <patrickmassot@free.fr>
2023-12-20 16:01:35 -0500

    Fix suggestion option and statement macros

--- Verbose/French/Statements.lean
+++ Verbose/French/Statements.lean
@@ -1,28 +1,32 @@
 import Verbose.Tactics.Initialize
 import Verbose.Tactics.Widget
 
+import ProofWidgets.Demos.Macro
+
 open Lean Meta Elab Command
 
 open Lean.Parser.Term (bracketedBinder)
 
-/- **TODO**  Allow empty Given of Assume. -/
+/- **TODO**  Allow empty Given of Assume.
+**FIXME**: Need better behavior for empty proofs. Currently you need to put sorry there.
+-/
 
 elab ("Exercice"<|>"Exemple") str
     "Données :" objs:bracketedBinder*
     "Hypothèses :" hyps:bracketedBinder*
     "Conclusion :" concl:term
     "Démonstration :" prf:tacticSeq "QED": command => do
-  let opts ← getOptions
-  if opts.getBool `verbose_widget then
-    dbg_trace "Yes"
-    elabCommand (← `(command|example $(objs ++ hyps):bracketedBinder* : $concl := by
-      with_suggestions
-      $prf))
+  if (← getOptions).getBool `verbose.suggestion_widget then
+    let tac : TSyntax `tactic ←
+    Lean.TSyntax.mkInfoCanonical <$>
+      `(tactic| with_suggestions
+                  $prf)
+    elabCommand (← `(command|example $(objs ++ hyps):bracketedBinder* : $concl := by {$tac}))
   else
     dbg_trace "No"
     elabCommand (← `(command|example $(objs ++ hyps):bracketedBinder* : $concl := by $prf))
 
-set_option verbose_widget true
+set_option verbose.suggestion_widget true
 
 Exercice "Test"
   Données : (n : Nat)
--- Verbose/Tactics/Initialize.lean
+++ Verbose/Tactics/Initialize.lean
@@ -1,5 +1,5 @@
 import Lean
 
-register_option verbose_widget : Bool := {
+register_option verbose.suggestion_widget : Bool := {
   defValue := false
   descr := "Show the suggestion widget in proofs." }
