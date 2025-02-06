4ba8424abedcf8f95e5a5f751eff90e05671ebd6
Patrick Massot <patrickmassot@free.fr>
2025-02-06 12:05:03 +0100

    Allow CQFD at the end of French proofs

--- Verbose/French/Statements.lean
+++ Verbose/French/Statements.lean
@@ -17,12 +17,12 @@ elab ("Exercice"<|>"Exemple") str
     "Données :" objs:bracketedBinder*
     "Hypothèses :" hyps:bracketedBinder*
     "Conclusion :" concl:term
-    tkp:"Démonstration :" prf?:(tacticSeq)? tkq:"QED" : command => do
+    tkp:"Démonstration :" prf?:(tacticSeq)? tkq:("QED" <|> "CQFD") : command => do
   mkExercise none objs hyps concl prf? tkp tkq
 
 elab ("Exercice-lemme"<|>"Lemme") name:ident str
     "Données :" objs:bracketedBinder*
     "Hypothèses :" hyps:bracketedBinder*
     "Conclusion :" concl:term
-    tkp:"Démonstration :" prf?:(tacticSeq)? tkq:"QED" : command => do
+    tkp:"Démonstration :" prf?:(tacticSeq)? tkq:("QED" <|> "CQFD") : command => do
   mkExercise (some name) objs hyps concl prf? tkp tkq
