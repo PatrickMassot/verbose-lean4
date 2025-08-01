74763b8d1002185193cdbb9eae69e766adc3a156
Kim Morrison <kim@tqft.net>
2025-08-01 10:28:35 +1000

    chore: bump toolchain to v4.19

--- Verbose/French/By.lean
+++ Verbose/French/By.lean
@@ -21,7 +21,7 @@ elab "assumption'" : tactic => assumption'
 
 macro "hypothèse" : term => `(by assumption')
 
-lemma le_le_of_abs_le {α : Type*} [LinearOrderedAddCommGroup α] {a b : α} : |a| ≤ b → -b ≤ a ∧ a ≤ b := abs_le.1
+lemma le_le_of_abs_le {α : Type*} [AddCommGroup α] [LinearOrder α] [IsOrderedAddMonoid α] {a b : α} : |a| ≤ b → -b ≤ a ∧ a ≤ b := abs_le.1
 
 lemma le_le_of_max_le {α : Type*} [LinearOrder α] {a b c : α} : max a b ≤ c → a ≤ c ∧ b ≤ c :=
 max_le_iff.1
